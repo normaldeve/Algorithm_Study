0c730cc7a4677d3300a1a7ebb0a729204f782e6a
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-12-24 18:24:04 +0900

    문제집 제출 여부 표시

--- ch0X02/junwoo/백준_10871_X보다_작은_수.java
+++ ch0X02/junwoo/백준_10871_X보다_작은_수.java
@@ -1,12 +1,31 @@
 package junwoo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  *
  * @author junnukim1007gmail.com
  * @date 25. 12. 24.
  */
 public class 백준_10871_X보다_작은_수 {
-    static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        while (N-- > 0) {
+            int a = Integer.parseInt(st.nextToken());
+
+            if (a < X) {
+                System.out.print(a + " ");
+            }
+        }
     }
 }
