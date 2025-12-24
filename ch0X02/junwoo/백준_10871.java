9e1584a91a433a4c41428b7380e7b9efb01e2ac5
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-12-24 18:36:07 +0900

    2557 통과

--- ch0X02/junwoo/백준_10871.java
+++ ch0X02/junwoo/백준_10871.java
@@ -6,11 +6,11 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- *
+ * name : X보다_작은_수
  * @author junnukim1007gmail.com
  * @date 25. 12. 24.
  */
-public class 백준_10871_X보다_작은_수 {
+public class 백준_10871 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
---
+++ ch0X02/junwoo/백준_2557.java
@@ -0,0 +1,14 @@
+package junwoo;
+
+/**
+ * name : Hello World
+ *
+ * @author junnukim1007gmail.com
+ * @date 25. 12. 24.
+ */
+public class 백준_2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
